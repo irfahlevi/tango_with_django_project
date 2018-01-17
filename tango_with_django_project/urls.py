7333c1f26384590b78eca8ab42c554a2dc80c26b
Imam Reiza Fahlevi <2274277r@student.gla.ac.uk>
2018-01-17 22:27:04 +0000

    finished all chap 3, ready for chap 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    
     url(r'^rango/', include('rango.urls')),
 
     #above maps any URLs starting with rango/ to be handled by the rango application
